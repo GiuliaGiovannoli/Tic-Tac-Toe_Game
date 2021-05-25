846bbf895a15153f57ed182dbce713c1a586fa53
Giulia Giovannoli <giovannoli.giulia@gmail.com>
2021-05-25 15:49:46 +0200

    game

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Game from './components/Game';
+import './App.css';
+
+const App = () => {
+return (
+  <>
+  <h2 style={{ color: 'darkblue', textAlign: 'center' }}>Tic Tac Toe</h2>
+  <br></br>
+  <Game />
+  </>
+)}
+
+export default App;
+
